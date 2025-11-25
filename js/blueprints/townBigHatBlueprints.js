9374d16326813221d29d95bf088870d39369b062
Hal <74992877+FrothyPizza@users.noreply.github.com>
2025-11-25 17:15:02 -0600

    bigHatBoss takes no backwards knockback to not mess up his strafe origin

--- js/blueprints/townBigHatBlueprints.js
+++ js/blueprints/townBigHatBlueprints.js
@@ -109,8 +109,8 @@ ECS.Blueprints.BigHatSmallHatProjectileInteract = function(other) {
         this.addComponent(new ECS.Components.RemoveFromScene(true));
 
         const dir = Math.sign(this.Velocity.x) || 1;
-        let duration = 180;
-        other.addComponent(new ECS.Components.Stunned({x: 0.33 * dir, y: -1.5}, 20, duration, false));
+        let duration = 120;
+        other.addComponent(new ECS.Components.Stunned({x: 0, y: -1.5}, 20, duration, false));
         
         // Apply damage/stun to boss
         // other.addComponent(new ECS.Components.Stunned(...)); 
