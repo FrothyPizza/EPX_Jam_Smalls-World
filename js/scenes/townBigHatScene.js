a2ee777e8ae048f0e9d0aa9da93df35b9078f6c7
Hal <74992877+FrothyPizza@users.noreply.github.com>
2025-11-29 08:41:07 -0600

    add animtains to hat

--- js/scenes/townBigHatScene.js
+++ js/scenes/townBigHatScene.js
@@ -93,6 +93,13 @@ class TownBigHatScene extends LevelScene {
 
                 //This doesn't work
                 //hat.AnimatedSprite.setAnimation("IdlePickUp")
+                // find the hat
+                const hats = ECS.getEntitiesWithComponents('BigHatHatState');
+                let hat = null;
+                if (hats.length > 0) {
+                    hat = hats[0];
+                    hat.AnimatedSprite.setAnimation("IdlePickUp");
+                }
 
                 bossEntity.AnimatedSprite.setAnimation('Death');
                 // hide the shotgun (AnimatedSprite.hidden = true)
